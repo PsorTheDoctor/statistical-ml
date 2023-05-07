675eeb64d6de6c4cb940ff9cd6ae7154883b14c0
PsorTheDoctor <psor2.0@gmail.com>
2023-05-07 16:15:29 +0200

    2d data loaders

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,6 +3,15 @@ import numpy as np
 from sklearn import utils
 
 
+def get_img(flatten_img):
+    dim = 18
+    img = np.zeros((dim, dim))
+    for x in range(dim):
+        for y in range(dim):
+            img[y, x] = flatten_img[y*dim - (x-1)]
+    return img
+
+
 def load_unsplitted_data(n_persons=10, shuffle=False):
     if n_persons in [1, 4, 10, 12]:
         data = pyreadr.read_r('data/data_{}.Rdata'.format(n_persons))
@@ -19,8 +28,37 @@ def load_unsplitted_data(n_persons=10, shuffle=False):
     return people, labels, images
 
 
+def load_2d_unsplitted_data(n_persons=10, shuffle=False):
+    people, labels, flatten_images = load_unsplitted_data(n_persons, shuffle)
+    images = []
+    for flatten_img in flatten_images:
+        img = get_img(flatten_img)
+        images.append(img)
+
+    images = np.asarray(images)
+    return people, labels, images
+
+
 def load_all_persons_in_dataset(n_persons=10, split_ratio=0.8, shuffle=False, verbose=False):
-    _, labels, images = load_unsplitted_data(n_persons, shuffle=shuffle)
+    _, labels, images = load_unsplitted_data(n_persons, shuffle)
+    n = images.shape[0]
+
+    X_train = images[:int(n * split_ratio), :]
+    y_train = labels[:int(n * split_ratio)]
+    X_test = images[int(n * split_ratio):, :]
+    y_test = labels[int(n * split_ratio):]
+
+    if verbose:
+        print('X_train shape:', X_train.shape)
+        print('y_train shape:', y_train.shape)
+        print('X_test shape:', X_test.shape)
+        print('y_test shape:', y_test.shape)
+
+    return X_train, y_train, X_test, y_test
+
+
+def load_2d_all_persons_in_dataset(n_persons=10, split_ratio=0.8, shuffle=False, verbose=False):
+    _, labels, images = load_2d_unsplitted_data(n_persons, shuffle)
     n = images.shape[0]
 
     X_train = images[:int(n * split_ratio), :]
@@ -38,7 +76,37 @@ def load_all_persons_in_dataset(n_persons=10, split_ratio=0.8, shuffle=False, ve
 
 
 def load_disjunct_dataset(n_persons=10, split_ratio=0.8, shuffle=False, verbose=False):
-    people, labels, images = load_unsplitted_data(n_persons, shuffle=shuffle)
+    people, labels, images = load_unsplitted_data(n_persons, shuffle)
+
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
+
+    for i in range(people.shape[0]):
+        if people[i] in np.arange(int(split_ratio * n_persons)):
+            X_train.append(images[i])
+            y_train.append(labels[i])
+        else:
+            X_test.append(images[i])
+            y_test.append(labels[i])
+
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+
+    if verbose:
+        print('X_train shape:', X_train.shape)
+        print('y_train shape:', y_train.shape)
+        print('X_test shape:', X_test.shape)
+        print('y_test shape:', y_test.shape)
+
+    return X_train, y_train, X_test, y_test
+
+
+def load_2d_disjunct_dataset(n_persons=10, split_ratio=0.8, shuffle=False, verbose=False):
+    people, labels, images = load_2d_unsplitted_data(n_persons, shuffle)
 
     X_train = []
     y_train = []
@@ -75,7 +143,3 @@ def cross_validation(images, labels, test_size=0.1):
     X_test = images[int((1 - test_size) * n):, :]
     y_test = labels[int((1 - test_size) * n):]
     return X_train, y_train, X_test, y_test
-
-
-# load_all_persons_in_dataset(verbose=True)
-# load_disjunct_dataset(verbose=True)

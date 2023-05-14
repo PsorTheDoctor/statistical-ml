39eafe1d61110613305ff3b0a08d6bf81ef108a0
PsorTheDoctor <psor2.0@gmail.com>
2023-05-14 19:50:18 +0200

    cc

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import utils
 from scipy.ndimage import gaussian_filter
 from skimage.transform import rotate
+import cv2
 
 
 def get_img(flatten_img):
@@ -31,7 +32,10 @@ def load_unsplitted_data(n_persons=10, shuffle=False):
     return people, labels, images
 
 
-def load_2d_unsplitted_data(n_persons=10, shuffle=False, gaussian_blur=False):
+def load_2d_unsplitted_data(n_persons=10,
+                            shuffle=False,
+                            gaussian_blur=False,
+                            denoise=False):
 
     people, labels, flatten_images = load_unsplitted_data(n_persons, shuffle)
     images = []
@@ -41,10 +45,17 @@ def load_2d_unsplitted_data(n_persons=10, shuffle=False, gaussian_blur=False):
 
     images = np.asarray(images)
 
+    if denoise:
+        denoised = []
+        for img in images:
+            img = remove_noise(img)
+            denoised.append(img)
+        images = np.asarray(denoised)
+
     if gaussian_blur:
         blurred = []
         for img in images:
-            img = gaussian_filter(img, sigma=0.5)
+            img = gaussian_filter(img, sigma=0.7)
             blurred.append(img)
         images = np.asarray(blurred)
 
@@ -55,10 +66,11 @@ def load_all_persons_in_dataset(n_persons=10,
                                 split_ratio=0.8,
                                 shuffle=False,
                                 gaussian_blur=False,
+                                denoise=False,
                                 verbose=False):
 
     X_train, y_train, X_test, y_test = load_2d_all_persons_in_dataset(
-        n_persons, split_ratio, shuffle, gaussian_blur, verbose=False
+        n_persons, split_ratio, shuffle, gaussian_blur, denoise, verbose=False
     )
     X_train = X_train.reshape((X_train.shape[0], 18 * 18))
     X_test = X_test.reshape((X_test.shape[0], 18 * 18))
@@ -76,9 +88,12 @@ def load_2d_all_persons_in_dataset(n_persons=10,
                                    split_ratio=0.8,
                                    shuffle=False,
                                    gaussian_blur=False,
+                                   denoise=False,
                                    verbose=False):
 
-    _, labels, images = load_2d_unsplitted_data(n_persons, shuffle, gaussian_blur)
+    _, labels, images = load_2d_unsplitted_data(
+        n_persons, shuffle, gaussian_blur, denoise
+    )
     n = images.shape[0]
 
     X_train = images[:int(n * split_ratio), :]
@@ -99,10 +114,11 @@ def load_disjunct_dataset(n_persons=10,
                           split_ratio=0.8,
                           shuffle=False,
                           gaussian_blur=False,
+                          denoise=False,
                           verbose=False):
 
     X_train, y_train, X_test, y_test = load_2d_disjunct_dataset(
-        n_persons, split_ratio, shuffle, gaussian_blur, verbose=False
+        n_persons, split_ratio, shuffle, gaussian_blur, denoise, verbose=False
     )
     X_train = X_train.reshape((X_train.shape[0], 18 * 18))
     X_test = X_test.reshape((X_test.shape[0], 18 * 18))
@@ -120,10 +136,12 @@ def load_2d_disjunct_dataset(n_persons=10,
                              split_ratio=0.8,
                              shuffle=False,
                              gaussian_blur=False,
+                             denoise=False,
                              verbose=False):
 
-    people, labels, images = load_2d_unsplitted_data(n_persons, shuffle, gaussian_blur)
-
+    people, labels, images = load_2d_unsplitted_data(
+        n_persons, shuffle, gaussian_blur, denoise
+    )
     X_train = []
     y_train = []
     X_test = []
@@ -161,13 +179,15 @@ def cross_validation(images, labels, test_size=0.1):
     return X_train, y_train, X_test, y_test
 
 
-def augment(images, labels, n_times=10, rotation_range=15, shift_range=2):
+def augment(images, labels, n_times=1, rotation_range=15, shift_range=3):
     X = []
     y = []
     for n in range(n_times):
         for i in range(images.shape[0]):
             # Rotation
-            rotated = rotate(images[i], angle=np.random.randint(-rotation_range, rotation_range))
+            rotated = rotate(images[i],
+                             angle=np.random.randint(-rotation_range, rotation_range),
+                             mode='reflect')
             # Shift
             shift_x, shift_y = np.random.randint(-shift_range, shift_range, size=2)
             shifted = np.roll(np.roll(rotated, shift_x, axis=1), shift_y, axis=0)
@@ -178,3 +198,29 @@ def augment(images, labels, n_times=10, rotation_range=15, shift_range=2):
         print('{}% augmented'.format(int((n + 1) / n_times * 100)))
 
     return np.asarray(X), np.asarray(y)
+
+
+def remove_noise(img):
+    """
+    Applies connected components to remove noise from an image.
+    """
+    _, binary = cv2.threshold(np.uint8(img), 0, 255, cv2.THRESH_TOZERO)
+    binary_inv = cv2.bitwise_not(binary)
+
+    n, labels, stats, centroids = cv2.connectedComponentsWithStats(binary_inv)
+    largest_obj_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+
+    mask = (labels == largest_obj_label).astype(np.uint8) * 255
+    mask_inv = cv2.bitwise_not(mask)
+
+    result = np.full_like(img, 255)
+    result[mask_inv == 0] = img[mask_inv == 0]
+    return result
+
+
+# import matplotlib.pyplot as plt
+# _, _, X = load_2d_unsplitted_data(n_persons=1)
+#
+# for i in range(10):
+#     plt.imshow(np.fliplr(remove_noise(X[100 * i])), cmap='gray')
+#     plt.show()
